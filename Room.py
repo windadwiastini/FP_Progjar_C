bb59e7e511528b4bf8963859a4ce2c5c82a9d24c
anne <anne14@mhs.if.its.ac.id>
2016-12-20 16:19:48 +0700

    replace

--- Room.py
+++ Room.py
@@ -11,6 +11,19 @@ class Room():
         self.ranjau_p2_siap = False
         self.p1_selesai = False
         self.p2_selesai = False
+        self.ranjau_p1_siap = False
+        self.ranjau_p2_siap = False
+        self.p1_selesai = False
+        self.p2_selesai = False
+        self.ladang_p1 = []
+        self.ladang_p2 = []
+        self.tebakan_p1 = []
+        self.tebakan_p2 = []
+        self.skor_p1 = 0
+        self.skor_p2 = 0
+        self.skorSudahDihitung = False
+        self.p1_gameover = False
+        self.p2_gameover = False
         
     def getNamaRoom(self):
         return self.nama
@@ -45,13 +58,66 @@ class Room():
             self.p2_selesai = bool
 
     def cekKesiapanRanjau(self):
-        if self.ranjau_p1_siap == True and self.ranjau_p2_siap == True:
+        if self.ranjau_p1_siap is True and self.ranjau_p2_siap is True:
             return True
         else:
             return False
 
     def cekKesiapanAkhir(self):
-        if self.p1_selesai == True and self.p1_selesai == True:
+        if self.p1_selesai is True and self.p2_selesai is True:
             return True
         else:
             return False
+
+    def cleanLadang(self):
+        self.ladang_p1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.ladang_p2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+    def setLadang(self, player, posisiRanjau):
+        if player == 1:
+            self.ladang_p1 = posisiRanjau
+        else:
+            self.ladang_p2 = posisiRanjau
+
+    def getLadang(self, pemain):
+        if pemain == 1:
+            return self.ladang_p1
+        else:
+            return self.ladang_p2
+
+    def updateSkor(self):
+        if not self.skorSudahDihitung:
+            for i in range(4):
+                if self.ladang_p1[self.tebakan_p2[i]] == 1:
+                    self.skor_p1 += 1
+                else:
+                    self.skor_p2 += 1
+
+                if self.ladang_p2[self.tebakan_p1[i]] == 1:
+                    self.skor_p2 += 1
+                else:
+                    self.skor_p1 += 1
+            self.skorSudahDihitung = True
+        else:
+            return
+
+    def setUpdateSkorFlag(self, bool):
+        self.skorSudahDihitung = bool
+
+    def getSkor(self, pemain):
+        if pemain == 1:
+            return self.skor_p1
+        else:
+            return self.skor_p2
+
+    def setTebakan(self, pemain, tebakan):
+        if pemain == 1:
+            self.tebakan_p1 = tebakan
+        else:
+            self.tebakan_p2 = tebakan
+
+    def cekGameOver(self):
+        if 0 in self.ladang_p1 or 0 in self.ladang_p2:
+            return False
+        else:
+            return True
